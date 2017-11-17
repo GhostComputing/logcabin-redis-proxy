20d8591168bf812741b0b11c2f23d1f966affbb8
zengda <zengda@yunba.io>
2017-11-17 11:07:45 +0800

    bug fix for integer return

---
+++ deps/simple_resp/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/johnzeng/logcabin-redis-proxy/deps/simple_resp
+# Build directory: /home/johnzeng/logcabin-redis-proxy/deps/simple_resp
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(decode_encode_test "./decode_encode_test")
